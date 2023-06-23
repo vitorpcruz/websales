d6ec8945675c4b144342d1567e116a91bc552efb
Vitor Pereira <vitor-pcruz@hotmail.com>
2023-06-23 19:25:18 -0300

    add context injections to app.program

--- src/WebSales.APP/Program.cs
+++ src/WebSales.APP/Program.cs
@@ -1,15 +1,17 @@
+using WebSales.Infra.DependecyInjections;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("WebSalesDb");
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDatabaseInjection(connectionString);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,3 +27,4 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
--- src/WebSales.Infra/Contexts/Context.cs
+++ src/WebSales.Infra/Contexts/Context.cs
@@ -9,5 +9,18 @@ namespace WebSales.Infra.Contexts
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public Context(DbContextOptions<Context> options) : base(options) {  }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            foreach (var property in builder.Model.GetEntityTypes().SelectMany(e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            builder.ApplyConfigurationsFromAssembly(typeof(Context).Assembly);
+
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) 
+                relationship.DeleteBehavior = DeleteBehavior.SetNull;
+
+            base.OnModelCreating(builder);
+        }
     }
 }
